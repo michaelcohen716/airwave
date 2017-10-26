dc089142af0b97ab4332854715ecf19fb2c3e5ee
Michael Cohen <michaelcohen716@gmail.com>
2017-10-26 15:30:35 -0400

    continue styling modal-right

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -44,41 +44,58 @@ class SessionForm extends React.Component {
 
   render(){
 
-    const submitText = this.props.formType === "login" ? "Login" : "Signup";
+    const submitText = this.props.formType === "login" ? "Log in" : "Sign up";
     return (
     <div className="session-form-container" onClick={e=> e.stopPropagation()}>
-        {this.renderErrors()}
           <div className="session-form">
             <section className="session-column">
-              <div className="fb-login">
-                <div className="fb-login-head">
-                  <span>Log in instantly with Facebook</span>
+              <div className="session-column-fb">
+                <div className="fb-login">
+                  <div className="fb-login-head">
+                    <span className="fb-login-instantly">Log in instantly with Facebook</span>
+                  </div>
+                  <span className="fb-login-recommended">(Recommended)</span>
+                  <button className="fb-login-button"></button>
+                  <ul className="fb-login-attributes">
+                    <li>Share what you love with your friends</li>
+                    <li>See the videos your friends want to share</li>
+                    <li>Improve your recommendations</li>
+                  </ul>
                 </div>
-                <button className="fb-login-button"></button>
-                <ul className="fb-login-attributes">
-                  <li>Share what you love with your friends</li>
-                  <li>See the videos your friends want to share</li>
-                  <li>Improve your recommendations</li>
-                </ul>
+            </div>
+            </section>
+
+            <section className="session-divider">
+              <span className="session-divider-line"></span>
+              <div className="session-divider-or">
+                <span>OR</span>
               </div>
             </section>
 
             <section className="session-column">
-              <form onSubmit={this.handleSubmit} className="session-form-box">
-                Log in to Airwave
-                <input type="text" value={this.state.username}
-                  placeholder="username"
-                  onChange={this.update('username')}
-                  className="session-box" />
+              <div className="session-column-airwave">
+                {this.renderErrors()}
+                <form onSubmit={this.handleSubmit} className="session-form-box">
+                  <span className="airwave-login-head">Log in to Airwave</span>
+                  
+                  <section className="session-inputs">
+                    <input type="text" value={this.state.username}
+                      placeholder="username"
+                      onChange={this.update('username')}
+                      className="session-box" />
 
-                <input type="password" value={this.state.password}
-                  placeholder="password"
-                  onChange={this.update('password')}
-                  className="session-box" />
+                    <input type="password" value={this.state.password}
+                      placeholder="password"
+                      onChange={this.update('password')}
+                      className="session-box" />
+                  </section>
 
-
-                <input type="submit" value={submitText} />
-              </form>
+                  <span className="forgot-password">
+                    Forgot your password/email?
+                  </span>
+                  <input type="submit" value={submitText} />
+                </form>
+              </div>
             </section>
           </div>
 
