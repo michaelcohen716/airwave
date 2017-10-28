267f08d757e34c779859570d2bac307a076f6e98
Michael Cohen <michaelcohen716@gmail.com>
2017-10-28 19:19:27 -0400

    add watchlist-index thumbs

--- frontend/components/watchlist/watchlist.jsx
+++ frontend/components/watchlist/watchlist.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import WatchlistItem from './watchlist_item';
 
 
 
@@ -12,8 +12,14 @@ const Watchlist = () => {
     <div className="watchlist-parent">
       <section className="watchlist-topbar">
         <div className="watchlist-topbar-title">
-          Michael's Watchlist 
+          Michael's Watchlist
         </div>
+      </section>
+      <section className="watchlist-index">
+        <WatchlistItem />
+        <WatchlistItem />
+        <WatchlistItem />
+        <WatchlistItem />
       </section>
     </div>
   );
---
+++ frontend/components/watchlist/watchlist_item.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class WatchlistItem extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    return (
+      <div className="watchlist-item-outer">
+        <div className="watchlist-item-thumb">
+
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default WatchlistItem;
