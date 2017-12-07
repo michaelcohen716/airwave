ec36b4f45c1673633f5d261cfcb3bde5e8a4455a
Michael Cohen <michaelcohen716@gmail.com>
2017-12-07 11:04:24 -0500

    dull out unused buttons

--- frontend/components/nav/nav_bar_dropdown_account.jsx
+++ frontend/components/nav/nav_bar_dropdown_account.jsx
@@ -13,22 +13,22 @@ const NavBarDropdownAccount = ({currentUser, logout, openModal}) => {
         <div className="dropdown-content-holder account-dropdown-holder">
 
           <div className="account-dropdown-expand" id="hover-longer">
-            <button className="dropdown-content-link" id="hover-longer">
+            <button className="dropdown-content-link dulled" id="hover-longer">
               Watchlist
             </button>
-            <button className="dropdown-content-link">
+            <button className="dropdown-content-link dulled">
               History
             </button>
-            <button className="dropdown-content-link">
+            <button className="dropdown-content-link dulled">
               Profiles
             </button>
           </div>
 
           <div className="account-dropdown-expand">
-            <button className="dropdown-content-link">
+            <button className="dropdown-content-link dulled">
               Account
             </button>
-            <button className="dropdown-content-link">
+            <button className="dropdown-content-link dulled">
               Help
             </button>
             <button className="dropdown-content-link" onClick={logout} >
